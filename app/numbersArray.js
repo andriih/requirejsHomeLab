0640a15ad184faee2af62b20db3841ab08bbe4db
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2017-02-22 11:42:29 +0200

    sorting algorithm added

--- app/numbersArray.js
+++ app/numbersArray.js
@@ -2,7 +2,7 @@ define([],function(){
 	function a(arr){
 		this.arr = arr;
 		return arr;
-		//return 'numbersArray.js';
+		
 	}
 
 	return{
--- app/sortArray.js
+++ app/sortArray.js
@@ -2,7 +2,19 @@
 define(['numbersArray'],function (numbersArray) {
     function sortedArray(){
     	//return sort;
-    		return numbersArray;
+    		var arrNew = numbersArray.arr;
+   var len = arrNew.length;
+   for (var i = len-1; i>=0; i--){
+     for(var j = 1; j<=i; j++){
+       if(arrNew[j-1]>arrNew[j]){
+           var temp = arrNew[j-1];
+           arrNew[j-1] = arrNew[j];
+           arrNew[j] = temp;
+        }
+     }
+   }
+   return arrNew;
+
     }
     return {
     	sort:sortedArray
