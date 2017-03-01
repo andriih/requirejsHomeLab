d5965e7171b11d876abd5b68ee818d138f219f82
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2017-03-01 13:04:55 +0200

    Search module inited

--- app/main.js
+++ app/main.js
@@ -1,5 +1,13 @@
-requirejs(['numbersArray','sortArray','searchArray'],function (numbersArray,getSortedArray) {
+requirejs(['numbersArray','sortArray','searchArray'],function (numbersArray,getSortedArray,searchInArray) {
 	//добавляєм масив і сортуєм його
-	console.log(getSortedArray.sorted(numbersArray.setArray([12,-16,8,45,2,3,4])));
-	//
+	//console.log(getSortedArray.sorted(numbersArray.setArray([12,-16,8,45,2,3,4])));
+
+	var sort = getSortedArray.sorted(numbersArray.setArray([12,-16,8,45,2,3,4]));
+	console.log(sort);//Array [ -16, 2, 3, 4, 8, 12, 45 ]
+	
+	//метод searchForElement(arr,el) arr- масив el-елемент який шукаєм
+	//повертаєм індекс шуканого елемента
+	var search = searchInArray.searchForElement(sort,8);
+	console.log(search);//4
 });
+
--- app/searchArray.js
+++ app/searchArray.js
@@ -1,8 +1,14 @@
 define([],function(){
-	function searchInArray(arrNew){
-		
+	function searchInArray(arrNew,arrItem){
+		var n = arrNew.length, i = 0;   
+		arrNew[n] = arrItem ;
+		    
+		while (arrNew[ i ] !== arrItem) i++;                            
+		    if (i < n) return i;
+		    else return -1;  
 	}
+
 	return{
-		search:searchInArray
+		searchForElement:searchInArray
 	}
 });
