d7a67f0ce0a05baca8c335eda9ece2645bef1817
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2017-03-01 12:37:44 +0200

    Search module inited

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
-requirejs(['numbersArray','sortArray'],function (numbersArray,getSortedArray) {
-	//console.log(numbersArray.setArray([12,-16,8,45,2,3,4]));
+requirejs(['numbersArray','sortArray','searchArray'],function (numbersArray,getSortedArray) {
+	//добавляєм масив і сортуєм його
 	console.log(getSortedArray.sorted(numbersArray.setArray([12,-16,8,45,2,3,4])));
-	//console.log(getSortedArray.sorted());
+	//
 });
